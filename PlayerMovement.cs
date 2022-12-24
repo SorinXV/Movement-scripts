3f1005485629576e4533afde4321aad9b5f5c0f3
sorinxv <sorinmartinescu06@gmail.com>
2022-12-24 23:47:11 +0100

    player can now move, implemented new bugs :)

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -13,6 +13,36 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    void Update()
+    {
+        // Get input from WASD keys
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // Calculate movement direction
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+
+        // Normalize the movement direction
+        movementDirection = movementDirection.normalized;
+
+        // Set movement speed
+        float movementSpeed = 5.0f;
+        movementDirection *= movementSpeed;
+
+        // Apply movement to the player's position
+        transform.position += movementDirection * Time.deltaTime;
+
+        // Rotate the player model to face the movement direction
+        transform.LookAt(transform.position + movementDirection);
+    }
+}
     }
 }
